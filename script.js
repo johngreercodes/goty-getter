76375563d808f1e4c1fb0d249499a0c558fc3970
John Greer <johnmorrisgreer+gh@gmail.com>
2021-12-15 20:28:44 -0500

    built button container and games container for easier DOM manipulation organization

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const apiKey = `20862006752d4ed99d821ff4b2a41c0c`
 const gamesUrl = `https://api.rawg.io/api/games`
+const buttonsContainer = document.body.querySelector(`.buttons`)
+const gamesContainer = document.body.querySelector(`.games`)
 
 const testBtn = document.createElement('button')
 testBtn.innerText = 'test connection 👂'
@@ -36,7 +38,7 @@ const twenty19 = async () => {
   for (let i = 0; i < 5; i++) {
     const game = document.createElement('p')
     game.innerText = `${getData.data.results[i].name}`
-    document.body.appendChild(game)
+    document.body.querySelector(`.games`).appendChild(game)
   }
 }
 
@@ -44,3 +46,28 @@ twenty19btn.addEventListener('click', ()=>{
   twenty19()
 })
 
+////////////// 2020 btn
+
+const twenty20btn = document.createElement('button')
+twenty20btn.innerText = '2020'
+document.body.appendChild(twenty20btn)
+
+const twenty20 = async () => {
+  const getData = await axios.get(gamesUrl, {
+    params: {
+      dates: `2020-01-01,2020-12-31`,
+      ordering: `-rating`,
+      key: apiKey
+    }
+  })
+  for (let i = 0; i < 5; i++) {
+    let game = document.createElement('p')
+    game.innerText = `${getData.data.results[i].name}`
+    document.body.querySelector(`.games`).appendChild(game)
+  }
+}
+
+twenty20btn.addEventListener('click', ()=>{
+  document.body.querySelecot
+  twenty20()
+})
