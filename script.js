ba352e4ae9e9c80b0eb6f71f30fc9cebfc122a3b
John Greer <johnmorrisgreer+gh@gmail.com>
2021-12-15 19:38:50 -0500

    now we're sending API results to the page instead of the console

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 const apiKey = `20862006752d4ed99d821ff4b2a41c0c`
 const gamesUrl = `https://api.rawg.io/api/games`
 
-const btn = document.body.querySelector('#get-games-button')
+const testBtn = document.createElement('button')
+testBtn.innerText = 'test connection 👂'
+document.body.appendChild(testBtn)
 
 const axiosTest = async () => {
   const getData = await axios.get('https://api.rawg.io/api/games', {
     params: {
+      dates: `2019-01-01,2019-12-31`,
+      ordering: `-rating`,
       key: apiKey
     }})
-  console.log(getData.status)
+  console.log(getData)
   }
 
-btn.addEventListener('click', () => {
+testBtn.addEventListener('click', () => {
   axiosTest()
 })
 
-//////////////
+////////////// 2019 btn
 
 const twenty19btn = document.createElement('button')
 twenty19btn.innerText = '2019'
@@ -30,10 +34,12 @@ const twenty19 = async () => {
     }
   })
   for (let i = 0; i < 5; i++) {
-    console.log(getData.data.results[i].name)      
+    document.querySelector('h3').innerText += getData.data.results[i].name
   }
 }
 
 twenty19btn.addEventListener('click', ()=>{
-  twenty19();
+  twenty19()
 })
+
+// document.querySelector('h3').innerText = 'bye'
